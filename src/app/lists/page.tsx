8e3cacb14eea744aa88770a2cb0c3e1ff665968f
EneCatalinM <ene.catalinionut@yahoo.com>
2025-04-15 19:29:25 +0300

    Add like feature

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,11 +1,27 @@
+import React from "react";
+import {
+    fetchCurrentUserLikeIds,
+    fetchLikedMembers,
+} from "../actions/likeActions";
+import ListsTab from "./ListTab";
 
-const Lists = () => {
+
+export default async function ListsPage({
+    searchParams,
+}: {
+    searchParams: { type: string };
+}) {
+    const likeIds = await fetchCurrentUserLikeIds();
+    const members = await fetchLikedMembers(
+        searchParams.type
+    );
 
     return (
         <div>
-            Lists
+            <ListsTab
+                members={members}
+                likeIds={likeIds}
+            />
         </div>
-    )
+    );
 }
-
-export default Lists
